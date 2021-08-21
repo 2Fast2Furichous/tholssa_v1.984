d55f52564f15dfe50df932d79d184ab60f142f0d
Furichous Jones IV <fjones5@my.westga.edu>
2021-08-21 19:50:54 -0400

    Setup Maven structures for frontend and backend.
    Created Maven file structure for building the front
    and back end seperately.

---
+++ code/backend/src/test/java/devops/AppTest.java
@@ -0,0 +1,20 @@
+package devops;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
