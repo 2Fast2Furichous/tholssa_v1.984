013736dabd7f2699102e29af0a98fb28f3523c48
Furichous Jones IV <fjones5@my.westga.edu>
2021-08-21 23:58:44 -0400

    Created basic login page using JFoenix.
    Using JFoenix for Material Design standard. Required modification
    of pom to fix obscure reflection handling error with java.lang.reflect,
    when using Maven.

--- code/frontend/src/main/java/devops/App.java
+++ code/frontend/src/main/java/devops/App.java
@@ -1,13 +1,51 @@
 package devops;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 /**
- * Hello world!
+ * Entry point for the program
  *
+ * @author Special Topics - DevOps
+ * @version Fall 2021
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+
+    private static final String WINDOW_TITLE = "THOLSSA v1.984";
+    private static final String GUI_RESOURCE = "/devops/view/LoginWindow.fxml";
+
+    /**
+     * JavaFX entry point.
+     *
+     * @precondition none
+     * @postcondition none
+     *
+     * @throws IOException
+     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource(App.GUI_RESOURCE));
+        Scene scene = new Scene(parent);
+        primaryStage.setTitle(WINDOW_TITLE);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+    /**
+     * Primary Java entry point.
+     *
+     * @precondition none
+     * @postcondition none
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        App.launch(args);
+    }
+
 }
---
+++ code/frontend/src/main/java/devops/view/LoginWindow.java
@@ -0,0 +1,33 @@
+package devops.view;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class LoginWindow {
+
+	@FXML
+	private JFXButton login;
+
+	@FXML
+	private JFXTextField username;
+
+	@FXML
+	private JFXPasswordField password;
+
+	@FXML
+	private JFXButton signUp;
+
+	@FXML
+	void handleLogin(ActionEvent event) {
+
+	}
+
+	@FXML
+	void handleSignUp(ActionEvent event) {
+
+	}
+
+}
