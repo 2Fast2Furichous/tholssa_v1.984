a972ed147a88f8ed1b221473eca1a67be14f85b7
Javon O <jonuigb1@my.westga.edu>
2021-08-30 22:23:58 -0400

    Added an updated scene builder view using JFX
    linked login to the main page, and now displays a button on the main canvas

--- code/frontend/src/main/java/devops/view/LoginWindow.java
+++ code/frontend/src/main/java/devops/view/LoginWindow.java
@@ -7,6 +7,8 @@ import javax.swing.plaf.RootPaneUI;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+
+import devops.utils.FXRouter;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
@@ -27,7 +29,10 @@ public class LoginWindow {
 	private JFXButton signUp;
 
 	@FXML
-	void handleLogin(ActionEvent event) {
+	void handleLogin(ActionEvent event) throws IOException{
+        FXRouter.register("main", "/devops/view/MainUI.fxml");
+        FXRouter.setAnimationType("fade", 5);
+        FXRouter.show("main");
 		
 	}
 
--- code/frontend/src/main/java/devops/view/MainWindow.java
+++ code/frontend/src/main/java/devops/view/MainWindow.java
@@ -1,50 +1,81 @@
 package devops.view;
 
-
 import com.jfoenix.controls.JFXButton;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class MainWindow {
 
-    private FXMLLoader loader;
+    @FXML
+    private ResourceBundle resources;
 
-    public MainWindow(){
-        this.loader = new FXMLLoader(getClass().getResource("MainUI.fxml"));
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane tholssaGraph;
+
+    @FXML
+    private JFXButton familyButton;
+
+    @FXML
+    private JFXButton friendButton;
+
+    @FXML
+    private JFXButton buisnessButton;
+
+    @FXML
+    private JFXButton spouseButton;
+
+    @FXML
+    private JFXButton saveButton;
+
+    @FXML
+    private JFXButton cancelButton;
+
+    @FXML
+    private JFXButton removeButton;
+
+    @FXML
+    private JFXButton addButton;
+
+    @FXML
+    void addBuisnessNode(ActionEvent event) {
+
     }
 
     @FXML
-    private JFXButton add;
+    void addFamilyNode(ActionEvent event) {
+        JFXButton familyNode = new JFXButton();
+        familyNode.setText("hello");
+        this.tholssaGraph.getChildren().add(familyNode);
+    }
 
     @FXML
-    private JFXButton remove;
-
-    @FXML
-    private JFXButton save;
-
-    @FXML
-    private JFXButton cancel;
-
-    @FXML
-    void addToTree(ActionEvent event){
+    void addFriendFamily(ActionEvent event) {
         
-    }
-
-    @FXML
-    void removeFromTree(ActionEvent event){
 
     }
 
     @FXML
-    void saveTree(ActionEvent event){
+    void addSpouseNode(ActionEvent event) {
 
     }
 
     @FXML
-    void cancelFromTree(ActionEvent event){
+    void initialize() {
+        assert tholssaGraph != null : "fx:id=\"tholssaGraph\" was not injected: check your FXML file 'MainUI.fxml'.";
+        assert familyButton != null : "fx:id=\"familyButton\" was not injected: check your FXML file 'MainUI.fxml'.";
+        assert friendButton != null : "fx:id=\"friendButton\" was not injected: check your FXML file 'MainUI.fxml'.";
+        assert buisnessButton != null : "fx:id=\"buisnessButton\" was not injected: check your FXML file 'MainUI.fxml'.";
+        assert spouseButton != null : "fx:id=\"spouseButton\" was not injected: check your FXML file 'MainUI.fxml'.";
+        assert saveButton != null : "fx:id=\"saveButton\" was not injected: check your FXML file 'MainUI.fxml'.";
+        assert cancelButton != null : "fx:id=\"cancelButton\" was not injected: check your FXML file 'MainUI.fxml'.";
+        assert removeButton != null : "fx:id=\"removeButton\" was not injected: check your FXML file 'MainUI.fxml'.";
+        assert addButton != null : "fx:id=\"addButton\" was not injected: check your FXML file 'MainUI.fxml'.";
 
     }
-
-    
 }
