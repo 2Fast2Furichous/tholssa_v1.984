18ed642cc7f141917a7a19a8004434bfbf15bf81
Javon O <jonuigb1@my.westga.edu>
2021-08-31 20:11:07 -0400

    Fixed the buttons jumping so far away
    The button no longer jumps really far away it drags along where the mouse is at

--- code/frontend/src/main/java/devops/view/MainWindow.java
+++ code/frontend/src/main/java/devops/view/MainWindow.java
@@ -10,6 +10,7 @@ import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainWindow {
 
@@ -56,15 +57,15 @@ public class MainWindow {
         this.tholssaGraph.getChildren().add(buisnessNode);
         buisnessNode.setOnMousePressed(new EventHandler<MouseEvent>(){
             public void handle(MouseEvent mouseEvent){
-               buisnessNode.setTranslateX(mouseEvent.getSceneX());
-               buisnessNode.setTranslateY(mouseEvent.getSceneY());
+               buisnessNode.setTranslateX(mouseEvent.getX());
+               buisnessNode.setTranslateY(mouseEvent.getY());
             }
         });
 
         buisnessNode.setOnMouseDragged(new EventHandler<MouseEvent>(){
             public void handle(MouseEvent mouseEvent){
-                buisnessNode.setTranslateX(mouseEvent.getScreenX()+ tholssaGraph.getScaleX());
-                buisnessNode.setTranslateY(mouseEvent.getScreenY()+ tholssaGraph.getScaleY());
+                buisnessNode.setTranslateX(mouseEvent.getScreenX()- 500);
+                buisnessNode.setTranslateY(mouseEvent.getScreenY()- 120);
             }
         });
     }
@@ -74,14 +75,48 @@ public class MainWindow {
     void addFamilyNode(ActionEvent event) {
         JFXButton familyNode = new JFXButton();
         familyNode.setText("family");
+        familyNode.setStyle("-fx-background-color: lime;");
+        familyNode.setTranslateX(100);
+        familyNode.setTranslateY(150);
         this.tholssaGraph.getChildren().add(familyNode);
+
+        familyNode.setOnMousePressed(new EventHandler<MouseEvent>(){
+            public void handle(MouseEvent mouseEvent){
+               familyNode.setTranslateX(mouseEvent.getSceneX());
+               familyNode.setTranslateY(mouseEvent.getSceneY());
+            }
+        });
+
+        familyNode.setOnMouseDragged(new EventHandler<MouseEvent>(){
+            public void handle(MouseEvent mouseEvent){
+                familyNode.setTranslateX(mouseEvent.getScreenX() -500);
+                familyNode.setTranslateY(mouseEvent.getScreenY()- 120);
+            }
+        });
     }
 
     @FXML
     void addFriendFamily(ActionEvent event) {
         JFXButton friendNode = new JFXButton();
         friendNode.setText("friend");
+        friendNode.setStyle("-fx-background-color: lime;");
+        friendNode.setTranslateX(0);
+        friendNode.setTranslateY(0);
         this.tholssaGraph.getChildren().add(friendNode);
+
+        friendNode.setOnMousePressed(new EventHandler<MouseEvent>(){
+            public void handle(MouseEvent mouseEvent){
+               friendNode.setTranslateX(mouseEvent.getSceneX());
+               friendNode.setTranslateY(mouseEvent.getSceneY());
+            }
+        });
+
+        friendNode.setOnMouseDragged(new EventHandler<MouseEvent>(){
+            public void handle(MouseEvent mouseEvent){
+                friendNode.setTranslateX(mouseEvent.getScreenX()-500);
+                friendNode.setTranslateY(mouseEvent.getScreenY()-120);
+            }
+        });
 
     }
 
@@ -89,7 +124,24 @@ public class MainWindow {
     void addSpouseNode(ActionEvent event) {
         JFXButton spouseNode = new JFXButton();
         spouseNode.setText("spouse");
+        spouseNode.setStyle("-fx-background-color: lime;");
+        spouseNode.setTranslateX(100);
+        spouseNode.setTranslateY(180);
         this.tholssaGraph.getChildren().add(spouseNode);
+
+        spouseNode.setOnMousePressed(new EventHandler<MouseEvent>(){
+            public void handle(MouseEvent mouseEvent){
+               spouseNode.setTranslateX(mouseEvent.getSceneX());
+               spouseNode.setTranslateY(mouseEvent.getSceneY());
+            }
+        });
+
+        spouseNode.setOnMouseDragged(new EventHandler<MouseEvent>(){
+            public void handle(MouseEvent mouseEvent){
+                spouseNode.setTranslateX(mouseEvent.getScreenX()-500);
+                spouseNode.setTranslateY(mouseEvent.getScreenY()-120);
+            }
+        });
 
     }
 
