4a2e8d00c009dc1861fb7b781d44148de8bf2724
Alexander Ayers <alexayers2000@gmail.com>
2021-09-03 01:13:50 -0400

    Added initial files
    Added in the initial files for the development of the new user UI. An empty FXML file and a code behind.

---
+++ code/frontend/src/main/java/devops/view/CreateAccountWindow.java
@@ -0,0 +1,17 @@
+package devops.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+/**
+ * Code Behind for the Create Account Window
+ * 
+ * @author Alexander Ayers
+ * @version Fall 2021
+ */
+public class CreateAccountWindow {
+    
+    @FXML
+    private GridPane createAccountWindowPane;
+
+}
