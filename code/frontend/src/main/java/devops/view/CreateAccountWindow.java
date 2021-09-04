6af7f42c38060e69425b1207637131220ff7e380
Alexander Ayers <alexayers2000@gmail.com>
2021-09-04 18:51:25 -0400

    Added in full New User GUI.
    Added to the FXML the full GUI for the New User, just needs implementation of codebehind.

--- code/frontend/src/main/java/devops/view/CreateAccountWindow.java
+++ code/frontend/src/main/java/devops/view/CreateAccountWindow.java
@@ -1,7 +1,13 @@
 package devops.view;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.GridPane;
+
 
 /**
  * Code Behind for the Create Account Window
@@ -10,8 +16,43 @@ import javafx.scene.layout.GridPane;
  * @version Fall 2021
  */
 public class CreateAccountWindow {
-    
+    //TODO Add in Listeners for Input Validation with Error Dialog.
     @FXML
     private GridPane createAccountWindowPane;
 
+    @FXML
+    private JFXTextField usernameTextField;
+
+    @FXML
+    private JFXTextField passwordTextField;
+
+    @FXML
+    private JFXTextField firstNameTextField;
+
+    @FXML
+    private JFXTextField lastNameTextField;
+
+    @FXML
+    private JFXTextField phoneNumberTextField;
+
+    @FXML
+    private JFXDatePicker dateOfBirthDatePicker;
+
+    @FXML
+    private JFXButton cancelButton;
+
+    @FXML
+    private JFXButton createAccountButton;
+
+    @FXML
+    void handleCancel(ActionEvent event) {
+            //TODO Transfer back to Login Screen and Clear Fields.
+    }
+
+    @FXML
+    void handleCreateAccount(ActionEvent event) {
+        //TODO Add User Service linking for Create Account.
+    }
+
 }
+
