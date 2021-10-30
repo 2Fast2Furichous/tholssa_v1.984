db2d4d971bb6fd17cd106b713513cdef7046f39a
Alexander Ayers <alexayers2000@gmail.com>
2021-10-30 17:51:04 -0400

    Added Review Class
    Added basic review class for use with PersonNode,.

---
+++ code/backend/src/main/java/devops/model/implementations/Review.java
@@ -0,0 +1,55 @@
+package devops.model.implementations;
+
+import devops.resources.ErrorMessages;
+
+/**
+ * Review of a Person
+ * 
+ * @version Fall 2021
+ * @author Alexander Ayers
+ */
+public class Review {
+    
+    private String name;
+    private String content;
+    private int score;
+
+    /**
+     * Three-paramater constructor.
+     * 
+     * @precondition
+     * @postcondition
+     * @param name
+     * @param content
+     * @param score
+     */
+    public Review(String name, String content, int score) {
+        if (name == null){
+            throw new IllegalArgumentException("Name cannot be null.");
+        }
+        if (name.isBlank()){
+            throw new IllegalArgumentException("Name cannot be blank.");
+        }
+        if(content == null){
+            throw new IllegalArgumentException("Content cannot be null.");
+        }
+        if(content.isBlank()){
+            throw new IllegalArgumentException("Content cannot be blank.");
+        }
+        if (score < 1 || score > 5){
+            throw new IllegalArgumentException("Score must be between 1 and 5.");
+        }
+        this.name = name;
+        this.content = content;
+        this.score = score;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getContent() {
+        return this.content;
+    }
+    public int getScore() {
+        return this.score;
+    }
+}
