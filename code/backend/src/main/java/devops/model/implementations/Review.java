c5bb9a31094879ff8be821d72deb9130ee863047
Alexander Ayers <alexayers2000@gmail.com>
2021-11-04 20:29:25 -0400

    Added Equals and Fully Tested Add Method
    Added equals to review for ease of testing the class, and additionally added testing to TestUpdateNode to reflect the reviews being updated. Add Review will be done via the UpdateNode method for sake of consistency with rest of person attributes.

--- code/backend/src/main/java/devops/model/implementations/Review.java
+++ code/backend/src/main/java/devops/model/implementations/Review.java
@@ -7,7 +7,9 @@ package devops.model.implementations;
  * @author Alexander Ayers
  */
 public class Review {
-    
+
+    private static final int MAXIMUM_SCORE = 5;
+    public static final int MINIMUM_SCORE = 1;
     private String name;
     private String content;
     private int score;
@@ -15,11 +17,11 @@ public class Review {
     /**
      * Three-paramater constructor.
      * 
-     * @precondition
-     * @postcondition
-     * @param name
-     * @param content
-     * @param score
+     * @precondition name != null && !name.isBlank() && content != null && !content.isBlank() && score >= 1 && score <= 5
+     * @postcondition getName() == name && getContent() == content && getScore() == score
+     * @param name the name.
+     * @param content the content.
+     * @param score the score.
      */
     public Review(String name, String content, int score) {
         if (name == null){
@@ -34,20 +36,59 @@ public class Review {
         if(content.isBlank()){
             throw new IllegalArgumentException("Content cannot be blank.");
         }
-        if (score < 1 || score > 5){
+        if (score < MINIMUM_SCORE || score > MAXIMUM_SCORE){
             throw new IllegalArgumentException("Score must be between 1 and 5.");
         }
         this.name = name;
         this.content = content;
         this.score = score;
     }
+
+    /**
+     * Gets the name.
+     * 
+     * @precondition none
+     * @postcondition none
+     * 
+     * @return the name
+     */
     public String getName() {
         return this.name;
     }
+
+    /**
+     * Gets the content.
+     * 
+     * @precondition none
+     * @postcondition none
+     * 
+     * @return the content
+     */
     public String getContent() {
         return this.content;
     }
+
+    /**
+     * Gets the score.
+     * 
+     * @precondition none
+     * @postcondition none
+     * 
+     * @return the score
+     */
     public int getScore() {
         return this.score;
+    }
+
+    @Override
+    public boolean equals(Object item){
+        if (item == null){
+            throw new IllegalArgumentException("The review cannot be null.");
+        }
+        if (!(item instanceof Review)){
+            throw new IllegalArgumentException("Comparison must be applied to object of type Review.");
+        }
+        var review = (Review) item;
+        return this.name.equals(review.getName()) && this.content.equals(review.getContent()) && this.score == review.getScore();
     }
 }
---
+++ code/backend/src/test/java/devops/model/review/TestEquals.java
@@ -0,0 +1,62 @@
+package devops.model.review;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import devops.model.implementations.Review;
+
+public class TestEquals {
+    private Review review = new Review("test", "test2", 2);
+
+    @Test
+    public void whenNullReviewExpectException(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            review.equals(null);
+        });
+    }
+
+    @Test
+    public void whenItemNotOfSameTypeExpectException(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            review.equals("test");
+        });
+    }
+
+    @Test
+    public void whenNameNotSameEverythingElseSameExpectFalse(){
+        assertFalse(review.equals(new Review("name", "test2", 2)));
+    }
+
+    @Test
+    public void whenContentNotSameEverythingElseSameExpectFalse(){
+        assertFalse(review.equals(new Review("test", "test", 2)));
+    }
+
+    @Test
+    public void whenScoreNotSameEverythingElseSameExpectFalse(){
+        assertFalse(review.equals(new Review("test", "test2", 1)));
+    }
+
+    @Test
+    public void whenScoreAndNameDifferentExpectFalse(){
+        assertFalse(review.equals(new Review("test1", "test2", 1)));
+    }
+
+    @Test
+    public void whenScoreAndContentDifferentExpectFalse(){
+        assertFalse(review.equals(new Review("test", "test", 1)));
+    }
+
+    @Test
+    public void whenContentAndNameDifferentExpectFalse(){
+        assertFalse(review.equals(new Review("test1", "test", 2)));
+    }
+
+    @Test
+    public void whenAllSameExpectTrue(){
+        assertTrue(review.equals(new Review("test", "test2", 2)));
+    }
+}
